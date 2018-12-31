5cfb69299438cc816b371478b24e8209daa45f6e
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-12-31 18:03:24 +0800

    Attempt rotation of STL image and saving image

--- training_data_generation/stl2image.py
+++ training_data_generation/stl2image.py
@@ -1,18 +1,39 @@
 from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
+import math
+import cv2
+import os
+
+GENERATED_FILES_BASE_LOC = "H:\\AR-ExtendingOnlineGames\\3d_pieces\\images"
 
 # Create a new plot
+# figure = pyplot.figure(frameon=False)
 figure = pyplot.figure()
 axes = mplot3d.Axes3D(figure)
 
 # Load the STL files and add the vectors to the plot
-your_mesh = mesh.Mesh.from_file('H:\\AR-ExtendingOnlineGames\\3d_pieces\\Chess_Set_-_Print_Friendly\\files\\Bishop.stl')
-axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+image_mesh = mesh.Mesh.from_file('H:\\AR-ExtendingOnlineGames\\3d_pieces\\Chess_Set_-_Print_Friendly\\files\\King.stl')
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(image_mesh.vectors))
 
 # Auto scale to the mesh size
-scale = your_mesh.points.flatten(-1)
+scale = image_mesh.points.flatten(-1)
 axes.auto_scale_xyz(scale, scale, scale)
+# axes.set_axis_off()
 
 # Show the plot to the screen
-pyplot.show()
+# pyplot.show()
+num_images_required = 3
+
+# 1 unit rotation in radians
+unit_rotation_radian = (2*math.pi) / num_images_required
+
+
+
+for itr in range(1, num_images_required + 1):
+    rotation_angle_radian = unit_rotation_radian * itr
+    image_mesh.rotate([0, 0, 1], rotation_angle_radian)
+    file_location = os.path.join(GENERATED_FILES_BASE_LOC, "img" + str(itr) + ".jpg")
+    pyplot.savefig(file_location)
+    print("Generated image...")
+    # pyplot.show()
