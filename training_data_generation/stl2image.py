51ee0c6055a2db821d44058b91cce719034a5936
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-12-31 22:35:52 +0800

    Add code to generate images of pieces in different orientations

--- training_data_generation/stl2image.py
+++ training_data_generation/stl2image.py
@@ -8,8 +8,8 @@ import os
 GENERATED_FILES_BASE_LOC = "H:\\AR-ExtendingOnlineGames\\3d_pieces\\images"
 
 # Create a new plot
-# figure = pyplot.figure(frameon=False)
-figure = pyplot.figure()
+figure = pyplot.figure(frameon=False)
+#figure = pyplot.figure()
 axes = mplot3d.Axes3D(figure)
 
 # Load the STL files and add the vectors to the plot
@@ -19,26 +19,17 @@ axes.add_collection3d(mplot3d.art3d.Poly3DCollection(image_mesh.vectors))
 # Auto scale to the mesh size
 scale = image_mesh.points.flatten(-1)
 axes.auto_scale_xyz(scale, scale, scale)
-# axes.set_axis_off()
+axes.set_axis_off()
 
-# Show the plot to the screen
-# pyplot.show()
-num_images_required = 3
+num_images_required = 6
 
-# 1 unit rotation in radians
-unit_rotation_radian = (2*math.pi) / num_images_required
+# 1 unit rotation
+unit_rotation = 360/num_images_required
 
-# check 90 degree rotation
-# image_mesh.rotate([0, 0, 1], math.pi/2)
-pyplot.show()
-# file_location = os.path.join(GENERATED_FILES_BASE_LOC, "img" + str(itr) + ".jpg")
-#pyplot.savefig(file_location)
-#print("Generated image...")
-
-""" for itr in range(1, num_images_required + 1):
-    rotation_angle_radian = unit_rotation_radian * itr
-    image_mesh.rotate([0, 0, 1], rotation_angle_radian)
+for itr in range(num_images_required):
+    rotation_angle = unit_rotation * itr
+    axes.view_init(azim=rotation_angle)
     file_location = os.path.join(GENERATED_FILES_BASE_LOC, "img" + str(itr) + ".jpg")
     pyplot.savefig(file_location)
     print("Generated image...")
-    # pyplot.show() """
+    # pyplot.show()
