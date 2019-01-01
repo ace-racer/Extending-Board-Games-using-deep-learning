ed40c071511d158be569e7a73c529ec125f3f400
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-01-01 13:19:55 +0800

    Add initial code for custom CNN

--- chess_piece_detection/app/models.py
+++ chess_piece_detection/app/models.py
@@ -1,8 +1,8 @@
 # Keras and TF imports
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
-from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Sequential, load_model, Model
+from keras.layers import Dense, GlobalAveragePooling2D, Activation, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from keras import backend as K
 from keras.applications.inception_v3 import preprocess_input
@@ -107,3 +107,63 @@ def train_InceptionV3_transfer_learning_model(X_train, y_train, X_test, y_test, 
         model.compile(loss='sparse_categorical_crossentropy', optimizer=SGD(lr=lrs[1], momentum=0.9), metrics=['accuracy'])
         return None, model
 
+def train_custom_cnn_model(X_train, y_train, X_test, y_test, model_configs, train_model = True):
+    model = Sequential()
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv),
+                     padding='valid',
+                     input_shape=shape_ord))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    
+    model.add(Conv2D(nb_filters * 2, (nb_conv, nb_conv)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    
+    model.add(Conv2D(nb_filters * 4, (nb_conv, nb_conv)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.summary()
+
+    # checkpoint
+    if not os.path.exists(model_folder_name):
+        os.makedirs(model_folder_name)
+    
+    filepath = os.path.join(model_folder_name, "custom_cnn.hdf5")
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1,
+                                 save_best_only=True,
+                                 mode='max')
+
+    earlystop = EarlyStopping(monitor='val_acc', min_delta=0.001, patience=10,
+                              verbose=1, mode='max')
+    
+    tensorboard = TensorBoard(log_dir=tensorboard_logs_folder_location, histogram_freq=0, write_graph=True, write_images=True)
+    
+    callbacks_list = [checkpoint, earlystop, tensorboard]
+    
+    adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=adam,
+                  metrics=['accuracy'])
+
+    hist = model.fit(X_train, Y_train, shuffle=True, batch_size=batch_size,
+                     epochs=nb_epoch, verbose=1,
+                     validation_data=(X_test, Y_test), callbacks=callbacks_list)
+
+    # Evaluating the model on the test data
+    score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test score:', score)
+    print('Test accuracy:', accuracy)
+    return hist, model
