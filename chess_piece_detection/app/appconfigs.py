0f3dc922df58b2bd755737c307e294e77b702f8d
issuser <issuser@ISS-STF112.stf.nus.edu.sg>
2019-01-02 22:25:24 +0800

    Update model configs

--- chess_piece_detection/app/appconfigs.py
+++ chess_piece_detection/app/appconfigs.py
@@ -2,8 +2,8 @@
 
 location_of_train_data = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\output_train"
 location_of_test_data = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\output_test"
-model_folder_location = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\trained_models\\01_01"
-tensorboard_logs_folder_location = "C:\\Users\issuser\\Desktop\\ExtendingBoardGamesOnline\\tb_logs\\01_01"
+model_folder_location = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\trained_models\\02_01"
+tensorboard_logs_folder_location = "C:\\Users\issuser\\Desktop\\ExtendingBoardGamesOnline\\tb_logs\\02_01"
 """
 location_of_train_data = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data\\output_train"
 location_of_test_data = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data\\output_test"
--- chess_piece_detection/app/modelconfigs.py
+++ chess_piece_detection/app/modelconfigs.py
@@ -1,3 +1,3 @@
 inceptionV3configs = { "epochs": [100, 100], "batch_size": [100, 100], "lr": [0.00001, 0.0001], "model_weights_file_name": ["chess_pieces_inceptionv3_p1.hdf5", "chess_pieces_inceptionv3_p2.hdf5"], "model_weights_file_to_use": 1   }
 customCNNconfigs = { "epochs": [150], "batch_size": [32], "lr": [0.0001], "model_weights_file_name": ["custom_cnn.hdf5"], "model_weights_file_to_use": 0   }
-alexNetconfigs = { "epochs": [150], "batch_size": [32], "lr": [0.0001], "model_weights_file_name": ["alexnet.hdf5"], "model_weights_file_to_use": 0   }
+alexNetconfigs = { "epochs": [150], "batch_size": [8], "lr": [0.0001], "model_weights_file_name": ["alexnet.hdf5"], "model_weights_file_to_use": 0   }
