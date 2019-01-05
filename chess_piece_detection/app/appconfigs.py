512eb4fb1e338e8abc8f2c1d8c422435b67dd9fe
issuser <issuser@ISS-STF112.stf.nus.edu.sg>
2019-01-06 00:14:51 +0800

    Correct folder names used

--- chess_piece_detection/app/appconfigs.py
+++ chess_piece_detection/app/appconfigs.py
@@ -1,13 +1,13 @@
 # set the location of the training and test images (change as required)
 
-location_of_train_data = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\output_train"
-location_of_test_data = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\output_test"
+location_of_train_data = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\train"
+location_of_test_data = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\test"
 model_folder_location = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\trained_models\\05_01"
 tensorboard_logs_folder_location = "C:\\Users\issuser\\Desktop\\ExtendingBoardGamesOnline\\tb_logs\\05_01"
 base_data_location = "C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data"
 """
-location_of_train_data = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data\\output_train"
-location_of_test_data = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data\\output_test"
+location_of_train_data = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data\\train"
+location_of_test_data = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data\\test"
 model_folder_location = "H:\\AR-ExtendingOnlineGames\\ExtendingGames_Code\\models\\01_19"
 tensorboard_logs_folder_location = "H:\\AR-ExtendingOnlineGames\\tb_logs\\01_19_inception"
 base_data_location = "H:\\AR-ExtendingOnlineGames\\data\\chess_pieces_data\\Chess ID Public Data"
