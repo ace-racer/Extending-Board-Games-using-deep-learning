11a1d26fbfdd37f1432f82759cdfe20ec24b410d
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-01-05 14:09:52 +0800

    Add initial code for encode, decode and remove log statements

--- API/utils.py
+++ API/utils.py
@@ -1,26 +1,28 @@
 from itertools import product
 import numpy as np
 import random
+import base64
 
 cols = list("abcdefgh")
 rows = list("12345678")
 cartesian_product = list(product(cols, rows))
-print(cartesian_product)
+
 board_positions = [x[0] + x[1] for x in cartesian_product]
-print(board_positions)
+
 chess_pieces = list("p"*8 + "n"*2 + "r"*2 + "b"*2 + "k" + "q")
-print(chess_pieces)
+
 black_pieces = ["b" + x for x in chess_pieces]
 white_pieces = ["w" + x for x in chess_pieces]
 all_pieces = black_pieces
 all_pieces.extend(white_pieces)
-print("All pieces...")
-print(all_pieces)
 
-def generate_dummy_board(num_pieces = 32):
+
+
+def generate_dummy_board(num_pieces=32):
     chosen_positions = random.sample(board_positions, num_pieces)
     chosen_pieces = random.sample(all_pieces, num_pieces)
     return dict(zip(chosen_positions, chosen_pieces))
+
 
 def simulate_move():
     moves = {}
@@ -28,3 +30,36 @@ def simulate_move():
     moves["d2"] = "empty"
     return moves
 
+
+def base64_encode_image(image):
+    """
+    Ref: https://www.pyimagesearch.com/2018/01/29/scalable-keras-deep-learning-rest-api/
+    Usage: 
+        d = {"id": k, "image": base64_encode_image(image)}
+        db.set("ID123", json.dumps(d))
+    """
+    # ensure our NumPy array is C-contiguous as well, otherwise we won't be able to serialize it
+    image = image.copy(order="C")
+
+    # base64 encode the input NumPy array
+    return base64.b64encode(image).decode("utf-8")
+
+def base64_decode_image(image_obj, shape, dtype="float32"):
+    """
+    Ref: https://www.pyimagesearch.com/2018/01/29/scalable-keras-deep-learning-rest-api/
+    Usage: 
+        item = json.loads(item.decode("utf-8"))
+        image = base64_decode_image(item["image"], (200, 200))
+    """
+
+    image = bytes(image_obj, encoding="utf-8")
+
+    # convert the string to a NumPy array using the supplied data type and target shape
+    image = np.frombuffer(base64.decodestring(image), dtype=dtype)
+
+    num_channels = 3
+    required_shape = (1, shape[0], shape[1], num_channels)
+    image = image.reshape(required_shape)
+
+    # return the decoded image
+    return image
