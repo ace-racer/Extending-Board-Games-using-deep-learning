d9e3faaf8f322f9304ff24ff87a2c81f7fb830b1
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-01-12 15:00:14 +0800

    Add class for request processing

---
+++ API/requestprocessor.py
@@ -0,0 +1,9 @@
+from redisprovider import RedisProvider
+from mongodbprovider import MongoDBProvider
+
+class RequestProcessor:
+    def __init__(self):
+        pass
+
+    def process_chess_board_image(self, move_number, game_id, chess_board_image):
+        pass
