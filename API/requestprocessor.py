be317a445051ba48bfdb9b8ce048f2d6842a078c
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-01-12 15:09:01 +0800

    Add code to store request in MongoDB

--- API/requestprocessor.py
+++ API/requestprocessor.py
@@ -1,9 +1,20 @@
 from redisprovider import RedisProvider
 from mongodbprovider import MongoDBProvider
+import utils
+import constants
+import configurations
 
 class RequestProcessor:
     def __init__(self):
-        pass
+        self._mongo_db_provider = MongoDBProvider()
+        self._redis_provider = RedisProvider()
 
     def process_chess_board_image(self, move_number, game_id, chess_board_image):
+
+        # Step 1: serialize the incoming image and store request object for persistence
+        serialized_chess_board_image = utils.base64_encode_image(chess_board_image)
+        request_obj = {"move_number": move_number, "game_id": game_id, "chess_board_image": serialized_chess_board_image}
+        self._mongo_db_provider.insert_record(request_obj, constants.request_chessboard_details_collection)
+
+        # Step 2: Segment the chess board image and get a list of images
         pass
