3dd5c68cdf1e3e53489023321a006cc35b839e78
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-02-06 13:36:18 +0800

    Updated cropping after 4 point transform

--- chess_board_segmentation/manual_segmentation/manual_segment.py
+++ chess_board_segmentation/manual_segmentation/manual_segment.py
@@ -5,6 +5,7 @@ import os
 
 SPLIT_IMAGES_LOCATION = "H:\\AR-ExtendingOnlineGames\\my_board\\split_images\\board1"
 CHESS_BOARD_IMAGE_LOCATION = "H:\\AR-ExtendingOnlineGames\\my_board\\boards\\board1.jpg"
+SQUARE_SIDE_LENGTH = 70
 
 if not os.path.exists(SPLIT_IMAGES_LOCATION):
     print("Creating folder to store split images {0}".format(SPLIT_IMAGES_LOCATION))
@@ -27,6 +28,12 @@ def split_board(img, create_files=True):
                 cv2.imwrite(os.path.join(SPLIT_IMAGES_LOCATION, position + ".jpg"), image)
     return arr
 
+def four_point_transform(img, points, square_length=SQUARE_SIDE_LENGTH):
+    board_length = square_length * 8
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0, 0], [0, board_length], [board_length, board_length], [board_length, 0]])
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    return cv2.warpPerspective(img, M, (board_length, board_length))
 
 
 img = cv2.imread(CHESS_BOARD_IMAGE_LOCATION)
@@ -65,6 +72,18 @@ corners = [
 corners = [(int(round(x[0])), int(round(x[1]))) for x in corners]
 print(corners)
 
+        
+for point in corners:
+    cv2.circle(resized, tuple(point), 5, (0,255,0), -1)
+
+cv2.imwrite(os.path.join(SPLIT_IMAGES_LOCATION, 'corner_points.jpg'), resized)
+
+transformed_board = four_point_transform(resized, corners)
+cv2.imwrite(os.path.join(SPLIT_IMAGES_LOCATION, 'transformed_board.jpg'), transformed_board)
+
+split_board(transformed_board)
+
+"""
 image_height = corners[2][1] - corners[0][1]
 image_width = corners[2][0] - corners[0][0]
 
@@ -84,3 +103,4 @@ split_board(cropped_image)
 
 #plt.imshow(cropped_image)
 #plt.show()
+"""
