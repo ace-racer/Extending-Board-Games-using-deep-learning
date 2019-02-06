b77070c5deec2ce07ad27ea475f8a15b1f29dec7
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-02-06 12:03:23 +0800

    Update code to segment the images

--- chess_board_segmentation/manual_segmentation/manual_segment.py
+++ chess_board_segmentation/manual_segmentation/manual_segment.py
@@ -1,10 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import os
 
-img = cv2.imread("H:\\AR-ExtendingOnlineGames\\my_board\\boards\\d2d0b33d-14e7-4a6d-ae5c-18f633bbf9f4.jpg")
+SPLIT_IMAGES_LOCATION = "H:\\AR-ExtendingOnlineGames\\my_board\\split_images\\board1"
+CHESS_BOARD_IMAGE_LOCATION = "H:\\AR-ExtendingOnlineGames\\my_board\\boards\\board1.jpg"
 
-scale_percent = 60 # percent of original size
+if not os.path.exists(SPLIT_IMAGES_LOCATION):
+    print("Creating folder to store split images {0}".format(SPLIT_IMAGES_LOCATION))
+    os.makedirs(SPLIT_IMAGES_LOCATION)
+
+def split_board(img, create_files=True):
+    """
+    Given a board image, returns an array of 64 smaller images.
+    """
+    row = "abcdefgh"
+    arr = []
+    sq_len = img.shape[0] // 8
+    for i in range(8):
+        for j in range(8):
+            image = img[i * sq_len : (i + 1) * sq_len, j * sq_len : (j + 1) * sq_len]
+            position = str(row[i]) + "_" + str(j+1)
+            arr.append({"image": image, "position": position})
+
+            if create_files:
+                cv2.imwrite(os.path.join(SPLIT_IMAGES_LOCATION, position + ".jpg"), image)
+    return arr
+
+
+
+img = cv2.imread(CHESS_BOARD_IMAGE_LOCATION)
+
+scale_percent = 99 # percent of original size
 
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
@@ -13,18 +40,41 @@ dim = (width, height)
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 print(resized.shape)
 
+# check the image after resize to get the corners
+#plt.imshow(resized)
+#plt.show()
+#exit(0)
+
 # clock wise from top left corner
+""" at scale 60
 corners = [
     (51.7124, 161.505),
     (479.469, 159.418),
     (487.815, 587.175),
     (55.8856, 593.43)
 ]
+"""
+corners = [
+    (84.25, 274.75),
+    (792.031, 264.426),
+    (804.974, 974.787),
+    (94.586, 985.12)
+]
 
 
-plt.imshow(resized)
-plt.show()
-#cv2.imshow("image", resized)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+corners = [(int(round(x[0])), int(round(x[1]))) for x in corners]
+print(corners)
 
+image_height = corners[2][1] - corners[0][1]
+image_width = corners[2][0] - corners[0][0]
+
+print("Height: " + str(image_height))
+print("Width: " + str(image_width))
+
+cropped_image = resized[corners[0][1]: corners[0][1] + image_height, corners[2][0] - image_width : corners[2][0]]
+cv2.imwrite(os.path.join(SPLIT_IMAGES_LOCATION, "cropped_board.jpg"), cropped_image)
+split_board(cropped_image)
+
+
+#plt.imshow(cropped_image)
+#plt.show()
