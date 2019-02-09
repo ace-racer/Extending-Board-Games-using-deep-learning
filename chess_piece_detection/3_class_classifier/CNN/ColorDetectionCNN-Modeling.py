cdfdc3d4b84b01932931dbdf23a41811b72aa403
issuser <issuser@ISS-STF112.stf.nus.edu.sg>
2019-02-09 14:18:28 +0800

    Add script for CNN model

---
+++ chess_piece_detection/3_class_classifier/CNN/ColorDetectionCNN-Modeling.py
@@ -0,0 +1,196 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import keras
+
+from keras.models import Sequential, load_model, Model
+from keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D, BatchNormalization
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras import optimizers
+from sklearn.metrics import confusion_matrix
+from tensorflow import set_random_seed
+set_random_seed(42)
+
+
+IMAGE_SIZE = (70, 70)
+def process_image(image_location):
+    image = cv2.imread(image_location)
+    
+    if image.shape[0] != IMAGE_SIZE[0] or image.shape[1] != IMAGE_SIZE[1]:
+        # print("Resizing the image: {0}".format(image_location))
+        resized_image = cv2.resize(image, IMAGE_SIZE, interpolation = cv2.INTER_AREA)
+    else:
+        resized_image = image
+           
+    return resized_image
+	
+def get_features_labels(data_path):
+    X, y = [], []
+    features_with_labels = []
+    for fol in os.listdir(data_path):
+        fol_path = os.path.join(data_path, fol)
+        if os.path.isdir(fol_path):
+            for file in os.listdir(fol_path):
+                if ".jpg" in file:
+                    label = -1
+
+                    if fol[0] == "b":
+                        label = 0
+                    elif fol[0] == "w":
+                        label = 1
+                    elif fol == "empty":
+                        label = 2
+
+                    if label != -1:
+                        complete_file_loc = os.path.join(fol_path, file)
+                        grayscale_image = process_image(complete_file_loc)
+                        features_with_labels.append({"feature": grayscale_image, "label": label})
+            
+                    
+    random.shuffle(features_with_labels)
+    #print(X[0][0])
+    #print(X[0][1])
+    X = [x["feature"] for x in features_with_labels]
+    y = [x["label"] for x in features_with_labels]
+
+    return np.array(X), np.array(y)
+	
+X_train, y_train = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\train")
+X_test, y_test = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\Chess ID Public Data\\test")
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+# batch size
+batch_size = 64
+
+# number of training epochs
+epochs = 150
+
+required_input_shape = (*IMAGE_SIZE, 3)
+
+model_folder_name = "models"
+tensorboard_logs_folder_location = "logs"
+
+def train_3_class_cnn_model(X_train, Y_train, X_test, Y_test):
+    """"""
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='valid', input_shape=required_input_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.6))
+    
+    model.add(Dense(3))
+    model.add(Activation('softmax'))
+    model.summary()
+
+    # checkpoint
+    if not os.path.exists(model_folder_name):
+        os.makedirs(model_folder_name)
+    
+    # tensorboard logs
+    if not os.path.exists(tensorboard_logs_folder_location):
+        os.makedirs(tensorboard_logs_folder_location)
+    
+    filepath = os.path.join(model_folder_name, "custom_cnn.hdf5")
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+    earlystop = EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=20, verbose=1, mode='max')
+    
+    tensorboard = TensorBoard(log_dir=tensorboard_logs_folder_location, histogram_freq=0, write_graph=True, write_images=True)
+    
+    callbacks_list = [checkpoint, earlystop, tensorboard]
+    
+    adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+    hist = model.fit(X_train, Y_train, shuffle=True, batch_size=batch_size,
+                     epochs=epochs, verbose=1,
+                     validation_data=(X_test, Y_test), callbacks=callbacks_list)
+
+    # Evaluating the model on the test data
+    score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test score:', score)
+    print('Test accuracy:', accuracy)
+    return hist, model
+	
+hist, model = train_3_class_cnn_model(X_train, y_train, X_test, y_test)
+
+# summarize history for accuracy
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
+## Helper method to print a confusion matrix
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+	
+plt.rcParams["figure.figsize"] = (10,10)
+class_names = ["black", "white", "empty"]
+test_predictions = model.predict(X_test, batch_size=batch_size)
+y_test_pred = [np.argmax(x) for x in test_predictions]
+cnf_matrix = confusion_matrix(y_test, y_test_pred)
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=False,title='Normalized confusion matrix')
+plt.show()
+
+
+X_test1, y_test1 = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\myboard_images")
+test_predictions1 = model.predict(X_test1, batch_size=batch_size)
+y_test_pred1 = [np.argmax(x) for x in test_predictions1]
+cnf_matrix1 = confusion_matrix(y_test1, y_test_pred1)
+plot_confusion_matrix(cnf_matrix1, classes=class_names, normalize=False,title='confusion matrix')
+plt.show()
