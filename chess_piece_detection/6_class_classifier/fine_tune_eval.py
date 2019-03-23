7a58179c49bba8437660e014ad18ac928d90b9bd
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-03-23 14:41:03 +0800

    Add code to fine tune as python script

---
+++ chess_piece_detection/6_class_classifier/fine_tune_eval.py
@@ -0,0 +1,294 @@
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import numpy as np
+import os
+import random
+from collections import Counter, defaultdict
+from itertools import product, combinations
+import math
+import cv2
+from sklearn.model_selection import train_test_split
+
+random.seed(42)
+
+import keras
+from keras.layers import Input, Conv2D, Lambda, average, Dense, Flatten,MaxPooling2D, BatchNormalization, Dropout, Activation, Subtract, subtract
+from keras.models import Model, Sequential
+from keras.regularizers import l2
+from keras import backend as K
+from keras.optimizers import SGD,Adam
+from keras.losses import binary_crossentropy
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import numpy.random as rng
+
+IMAGE_SIZE = (70, 70)
+
+def auto_canny(image, sigma=0.33):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+ 
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	# return the edged image
+	return edged
+
+def process_image(image_location):
+    """
+        Given the image location, process the image
+    """
+    # print(image_location)
+    
+    image = cv2.imread(image_location)
+    
+    if image.shape[0] != IMAGE_SIZE[0] or image.shape[1] != IMAGE_SIZE[1]:
+        # print("Resizing the image: {0}".format(image_location))
+        resized_image = cv2.resize(image, IMAGE_SIZE, interpolation = cv2.INTER_AREA)
+    else:
+        resized_image = image
+    
+    gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+    
+    # get the edges from the image
+    #edges = auto_canny(gray)
+    #print(edges.shape)
+    
+    
+    # assert(denoised != edges)
+    # add the two images in a weighted manner
+    # weighted_sum = cv2.addWeighted(gray, 0.8, edges, 0.2, 0)
+       
+    return gray
+
+type_locations = {"b": ["bb", "wb"], "n": ["bn", "wn"], "k": ["bk", "wk"], "p": ["bp", "wp"], "q": ["bq", "wq"], "r": ["br", "wr"]}
+type_name_to_label = { "p":0, "b":1, "n":2, "r":3, "q": 4, "k":5 }
+
+
+def get_features_labels(data_path):
+    X, y = [], []
+    features_with_labels = []
+    
+    for type_name in type_locations:
+        for folder_name in type_locations[type_name]:
+            piece_type_folder = os.path.join(data_path, folder_name)
+            for f in (os.listdir(piece_type_folder)):
+                if f.endswith(".jpg"): 
+                    
+
+                    img_file_loc = os.path.join(piece_type_folder, f)
+                    print(img_file_loc)
+                    grayscale_image = process_image(img_file_loc)
+                    actual_image = grayscale_image
+                    grayscale_image = grayscale_image[..., np.newaxis]
+                    label = type_name_to_label[type_name]
+                    features_with_labels.append({"feature": grayscale_image, "label": label, "image": actual_image})   
+                    
+    random.shuffle(features_with_labels)
+    #print(X[0][0])
+    #print(X[0][1])
+    X = [x["feature"] for x in features_with_labels]
+    y = [x["label"] for x in features_with_labels]
+    images = [x["image"] for x in features_with_labels]
+    
+    X = np.array(X)
+    X = X.astype('float32')
+    X /= 255
+
+    return X, np.array(y), images
+
+
+X_train, y_train, _ = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\sriraj_v2\\train")
+X_test, y_test, test_images = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\sriraj_v2\\test")
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+
+# batch size
+batch_size = 64
+
+
+# number of training epochs
+epochs = 500
+
+required_input_shape = (*IMAGE_SIZE, 1)
+
+model_folder_name = "fine_tune/models"
+tensorboard_logs_folder_location = "fine_tune/logs"
+
+
+def load_6_class_cnn_model(model_weights_location):
+    """"""
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='valid', input_shape=required_input_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(128, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    
+    model.add(Dense(2048))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.6))
+    
+    model.add(Dense(6))
+    model.add(Activation('softmax'))
+    model.summary()
+
+    # load the model weights
+    model.load_weights(model_weights_location)
+                           
+    adam = Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    
+    return model
+
+
+trained_model = load_6_class_cnn_model("models/6_class_cnn.hdf5")
+
+
+# checkpoint
+if not os.path.exists(model_folder_name):
+    os.makedirs(model_folder_name)
+
+# tensorboard logs
+if not os.path.exists(tensorboard_logs_folder_location):
+    os.makedirs(tensorboard_logs_folder_location)
+
+filepath = os.path.join(model_folder_name, "6_class_cnn_fine_tuned.hdf5")
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+earlystop = EarlyStopping(monitor='val_acc', min_delta=0.00001, patience=40, verbose=1, mode='max')
+
+tensorboard = TensorBoard(log_dir=tensorboard_logs_folder_location, histogram_freq=0, write_graph=True, write_images=True)
+
+callbacks_list = [checkpoint, earlystop, tensorboard]
+
+adam = Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+trained_model.compile(loss='sparse_categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+hist = trained_model.fit(X_train, y_train, shuffle=True, batch_size=batch_size,
+                 epochs=epochs, verbose=1,
+                 validation_data=(X_test, y_test), callbacks=callbacks_list)
+
+# Evaluating the model on the test data
+score, accuracy = trained_model.evaluate(X_test, y_test, verbose=0)
+print('Test score:', score)
+print('Test accuracy:', accuracy)
+
+## Helper method to print a confusion matrix
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    
+    
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = [15, 15]
+
+IMAGES_PER_ROW = 5
+
+def plot_mispredicted_images(images, actual_values, predicted_values, mapping_func = None):
+    print(actual_values)
+    mis_predictions = actual_values ^ predicted_values
+    mis_prediction_indices = np.nonzero(mis_predictions)[0]
+    print(mis_prediction_indices)
+    
+    num_failed_images = len(mis_prediction_indices)
+    
+    if num_failed_images == 0:
+        print("All images predicted correctly.")
+        return
+        
+    
+    if num_failed_images == 1:
+        print("{0} mispredicted as {1}".format(actual_values[mis_prediction_indices[0]], predicted_values[mis_prediction_indices[0]]))
+        plt.imshow(images[mis_prediction_indices[0]], cmap='gray')
+        return
+    elif num_failed_images < IMAGES_PER_ROW:
+        num_images_per_row = 2
+    else:
+        num_images_per_row = IMAGES_PER_ROW
+        
+    num_rows = (num_failed_images // num_images_per_row) + int((num_failed_images % num_images_per_row) != 0)
+    if num_rows == 1:
+        num_rows = 2
+
+    print("Number of failed images: " + str(num_failed_images))
+    print("Num rows: {0}. Num images/row: {1}".format(num_rows, num_images_per_row))
+    #print(num_rows)
+    #print(num_images_per_row)
+
+
+    fig, axes = plt.subplots(num_rows, num_images_per_row)
+
+    current_image_idx = 0
+
+    for itr in range(num_rows):
+        #print(itr)
+        for jtr in range(num_images_per_row):
+            if current_image_idx == num_failed_images:
+                break
+            
+            print("{0}, {1}, {2}".format(itr, jtr, current_image_idx))
+            axes[itr, jtr].imshow(images[mis_prediction_indices[current_image_idx]], cmap='gray')
+            if mapping_func:
+                axes[itr, jtr].set_title("{0} predicted as {1}".format(mapping_func(actual_values[mis_prediction_indices[current_image_idx]]), mapping_func(predicted_values[mis_prediction_indices[current_image_idx]])))
+            else:
+                axes[itr, jtr].set_title("{0} predicted as {1}".format(actual_values[mis_prediction_indices[current_image_idx]], predicted_values[mis_prediction_indices[current_image_idx]]))
+            #print(current_image_idx)
+            current_image_idx += 1
