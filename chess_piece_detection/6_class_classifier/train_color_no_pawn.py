d9ae1664795c617060cb93cfd0b15f783b1360e1
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-03-24 21:57:20 +0800

    Add code to train model without pawn

---
+++ chess_piece_detection/6_class_classifier/train_color_no_pawn.py
@@ -0,0 +1,171 @@
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import numpy as np
+import os
+import random
+from collections import Counter, defaultdict
+from itertools import product, combinations
+import math
+import cv2
+from sklearn.model_selection import train_test_split
+
+random.seed(42)
+
+import keras
+from keras.layers import Input, Conv2D, Lambda, average, Dense, Flatten,MaxPooling2D, BatchNormalization, Dropout, Activation, Subtract, subtract
+from keras.models import Model, Sequential
+from keras.regularizers import l2
+from keras import backend as K
+from keras.optimizers import SGD,Adam
+from keras.losses import binary_crossentropy
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import numpy.random as rng
+
+IMAGE_SIZE = (70, 70)
+
+
+def process_image(image_location):
+    """
+        Given the image location, process the image
+    """
+    # print(image_location)
+    
+    image = cv2.imread(image_location)
+    
+    if image.shape[0] != IMAGE_SIZE[0] or image.shape[1] != IMAGE_SIZE[1]:
+        # print("Resizing the image: {0}".format(image_location))
+        resized_image = cv2.resize(image, IMAGE_SIZE, interpolation = cv2.INTER_AREA)
+    else:
+        resized_image = image
+       
+    return resized_image
+
+type_locations = {"b": ["bb", "wb"], "n": ["bn", "wn"], "k": ["bk", "wk"], "q": ["bq", "wq"], "r": ["br", "wr"]}
+type_name_to_label = { "b":0, "n":1, "r":2, "q": 3, "k":4 }
+
+
+def get_features_labels(data_path):
+    X, y = [], []
+    features_with_labels = []
+    
+    for type_name in type_locations:
+        for folder_name in type_locations[type_name]:
+            piece_type_folder = os.path.join(data_path, folder_name)
+            for f in (os.listdir(piece_type_folder)):
+                if f.endswith(".jpg"): 
+                    
+
+                    img_file_loc = os.path.join(piece_type_folder, f)
+                    print(img_file_loc)
+                    processed_image = process_image(img_file_loc)
+                    actual_image = processed_image
+                    label = type_name_to_label[type_name]
+                    features_with_labels.append({"feature": processed_image, "label": label, "image": actual_image})   
+                    
+    random.shuffle(features_with_labels)
+    #print(X[0][0])
+    #print(X[0][1])
+    X = [x["feature"] for x in features_with_labels]
+    y = [x["label"] for x in features_with_labels]
+    images = [x["image"] for x in features_with_labels]
+    
+    X = np.array(X)
+    X = X.astype('float32')
+    X /= 255
+
+    return X, np.array(y), images
+
+
+X_train, y_train, _ = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\train")
+X_test, y_test, test_images = get_features_labels("C:\\Users\\issuser\\Desktop\\ExtendingBoardGamesOnline\\data\\sriraj_v2\\test")
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+
+# batch size
+batch_size = 64
+
+
+# number of training epochs
+epochs = 500
+
+required_input_shape = (*IMAGE_SIZE, 3)
+
+model_folder_name = "models"
+tensorboard_logs_folder_location = "logs"
+
+
+def get_5_class_cnn_model_structure():
+    """"""
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='valid', input_shape=required_input_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(128, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    
+    model.add(Dense(2048))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.6))
+    
+    model.add(Dense(5))
+    model.add(Activation('softmax'))
+    model.summary()
+    
+    return model
+
+
+model = get_5_class_cnn_model_structure()
+
+
+# checkpoint
+if not os.path.exists(model_folder_name):
+    os.makedirs(model_folder_name)
+
+# tensorboard logs
+if not os.path.exists(tensorboard_logs_folder_location):
+    os.makedirs(tensorboard_logs_folder_location)
+
+filepath = os.path.join(model_folder_name, "5_class_color_cnn.hdf5")
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+earlystop = EarlyStopping(monitor='val_acc', min_delta=0.00001, patience=50, verbose=1, mode='max')
+
+tensorboard = TensorBoard(log_dir=tensorboard_logs_folder_location, histogram_freq=0, write_graph=True, write_images=True)
+
+callbacks_list = [checkpoint, earlystop, tensorboard]
+
+adam = Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(loss='sparse_categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+hist = model.fit(X_train, y_train, shuffle=True, batch_size=batch_size,
+                 epochs=epochs, verbose=1,
+                 validation_data=(X_test, y_test), callbacks=callbacks_list)
+
+# Evaluating the model on the test data
+score, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print('Test score:', score)
+print('Test accuracy:', accuracy)
